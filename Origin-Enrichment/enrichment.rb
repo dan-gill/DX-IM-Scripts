188e333538fac167ac544a511847129ab1f8c792
Dan Gill <dan.gill@dgillconsulting.com>
2020-07-13 22:39:50 +0100

    :sparkles: Add pollagent processing

--- Origin-Enrichment/enrichment.rb
+++ Origin-Enrichment/enrichment.rb
@@ -19,6 +19,7 @@ if File.file?('F:\Nimsoft\probes\slm\qos_processor\lib\sqljdbc6-6.2.2.jar')
    user="***REMOVED***"
    passwd="***REMOVED***"
    vmware_query = "SELECT DISTINCT cma.description, cmao.origin, sqs.source FROM cm_account cma JOIN cm_account_ownership cmao ON cma.account_id = cmao.account_id JOIN CM_DEVICE_ATTRIBUTE cmda ON cmda.dev_attr_value = cma.description JOIN CM_CONFIGURATION_ITEM cmci ON cmda.dev_id = cmci.dev_id JOIN CM_CONFIGURATION_ITEM_METRIC cmcim ON cmci.ci_id = cmcim.ci_id JOIN S_QOS_DATA sqs ON cmcim.ci_metric_id = sqs.ci_metric_id WHERE cmda.dev_attr_key = 'vmware.ResourcePoolvAppPath' AND sqs.source NOT LIKE '[0-9]%' AND sqs.source = '"
+   snmp_query = "SELECT DISTINCT cmao.origin, cma.name FROM cm_account cma JOIN cm_account_ownership cmao ON cma.account_id = cmao.account_id WHERE LEFT(cma.name, IIF(CHARINDEX(' ', cma.name)>0,CHARINDEX(' ', cma.name)-1,0)) = '"
    #==========================================================================#
 
 
@@ -37,7 +38,8 @@ if File.file?('F:\Nimsoft\probes\slm\qos_processor\lib\sqljdbc6-6.2.2.jar')
 
       b = con.create_statement
 
-      if ($monitor.probe == 'vmware')
+      case $monitor.probe
+      when 'vmware'
 
          vmware_query = vmware_query + $monitor.source + "' AND sqs.target = '" + $monitor.target + "' AND sqs.qos = '" +$monitor.qos + "' AND sqs.robot = '" + $monitor.robot + "';"
 
@@ -54,6 +56,39 @@ if File.file?('F:\Nimsoft\probes\slm\qos_processor\lib\sqljdbc6-6.2.2.jar')
          end
 
          rs.close
+      # Match SNMPcollector for ohos-snmp01 or txos-snmp01 ONLY
+      when 'pollagent'
+         if ($monitor.robot[/(?:oh|tx)os-snmp01/])
+            # Change source to all caps
+            # Look for two common naming standards: Starts with C/L/R and a number
+            # or starts with three character Mnemonic, then a dash
+            case $monitor.source.upcase
+            when /^[CLR]\d-/
+               mnemonic =  $monitor.source.upcase[/^[CLR]\d-(?<mnemonic>\w{2,3})/, "mnemonic"]
+            when /^\w{3}-/
+               mnemonic =  $monitor.source.upcase[/^(?<mnemonic>\w{3})-/, "mnemonic"]
+            else
+               mnemonic = nil
+            end
+
+            if (!mnemonic.nil?)
+               snmp_query = snmp_query + mnemonic + "';"
+               $logger.info("@@@ Running query: " + snmp_query)
+               rs=b.execute_query(snmp_query)
+
+               while(rs.next())
+                  if(!$monitor.origin.nil?)
+                     $logger.info("@@@ Probe: " + $monitor.probe + ", Source: " + $monitor.source + ", Target: " + $monitor.target + ", Old Origin: "+$monitor.origin)
+                     value=rs.getString("origin")
+                     $monitor.origin=value
+                     $logger.info("@@@ Probe: " + $monitor.probe + ", Source: " + $monitor.source + ", Target: " + $monitor.target + ", New Origin: "+$monitor.origin)
+                  end
+               end
+
+               rs.close
+            end
+
+         end
       end
 
       con.close
