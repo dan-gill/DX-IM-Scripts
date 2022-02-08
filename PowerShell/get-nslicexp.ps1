11362e25a4eebc59abdb0932b4efb90cefd47d8d
Dan Gill <dan.gill@dgillconsulting.com>
2022-02-08 17:02:47 -0600

    :sparkles: Update parameters
    Update parameters

--- PowerShell/get-nslicexp.ps1
+++ PowerShell/get-nslicexp.ps1
@@ -1,63 +1,56 @@
-﻿<#PSScriptInfo
-
-.VERSION 1.0
-
-.AUTHOR @dan_gill
-
-.COMPANYNAME gocloudwave.com
-
-.COPYRIGHT 2019
-
-.ICONURI 
-
-.EXTERNALMODULEDEPENDENCIES 
-
-.REQUIREDSCRIPTS 
-
-.EXTERNALSCRIPTDEPENDENCIES 
-
-.RELEASENOTES
-04-29-2019: Initial creation
-
-#> 
-
-<#
+﻿<#
 .SYNOPSIS
    Grabs Netscaler license information via PS
 .DESCRIPTION
    Grabs Netscaler license information via PS.
+.NOTES
+    File Name  : get-nslicexp.ps1
+    Author     : Dan Gill - dgill@gocloudwave.com
+.INPUTS
+   NetScaler IP, Admin user, Admin password as Secure String
+.OUTPUTS
+   Outputs license expiration
 .PARAMETER nsip
    DNS Name or IP of the Netscaler that needs to be configured. (MANDATORY)
 .PARAMETER adminaccount
-   Netscaler admin account (Default: ***REMOVED***)
+   Netscaler admin account (MANDATORY)
 .PARAMETER adminpassword
-   Password for the Netscaler admin account (Default: ***REMOVED***)
+   Password for the Netscaler admin account (MANDATORY)
 .EXAMPLE
-   ./get-NSlicexp -nsip 10.1.1.2
-.EXAMPLE
-   ./get-NSlicexp -nsip 10.1.1.2 | ft -AutoSize
-.EXAMPLE
-   ./get-NSlicexp -nsip 10.1.1.2 -adminaccount nsadmin -adminpassword "mysupersecretpassword"
-   #>
-   
-#Install-Module -Name NetScaler -Force
+   ./get-NSlicexp -nsip 10.1.1.2 -adminaccount nsadmin -adminpassword (ConvertTo-SecureString "mysupersecretpassword" -AsPlainText -Force)
+#>
+
+# Install NetScaler PowerShell module if not installed
+if (!(Get-Module -ListAvailable -Name NetScaler)) {
+    Install-Module -Name NetScaler -Scope CurrentUser
+}
 
 Param
 (
-    [Parameter(Mandatory=$true)]$nsip,
-    [String]$adminaccount = "***REMOVED***",
-    [String]$adminpassword = "***REMOVED***"
+   [Parameter(Mandatory=$true,
+              ValueFromPipelineByPropertyName=$true,
+              Position=0)]
+   [string]$nsip,
+   [Parameter(Mandatory=$true,
+              ValueFromPipelineByPropertyName=$true,
+              Position=1)]
+   [ValidateNotNullOrEmpty()]
+   [String]$adminaccount,
+   [Parameter(Mandatory=$true,
+              ValueFromPipelineByPropertyName=$true,
+              Position=2)]
+   [ValidateNotNullOrEmpty()]
+   [Security.SecureString]$adminpassword = $(Throw 'Password required.')
 )
 
-$User = $adminaccount
-$PWord = ConvertTo-SecureString -String $adminpassword -AsPlainText -Force
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminaccount, $adminpassword
 
 Connect-NetScaler -NSIP $nsip -Credential $Credential
 
 $output = Get-NSLicenseExpiration
-$output = $output -split "\r?\n"
+$output = $output -split '\r?\n'
 
 $output
 
+Disconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScalerDisconnect-NetScaler
 Disconnect-NetScaler
